c83e0bcd66c6efc4ab57b82369a3228c313ba69a
SudeepTimalsina <sudeeptimalsina18@gmail.com>
2020-12-20 00:16:45 +0545

    Update in firebase function and gitignore

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Layout from '../components/layout';
 import fetch from 'isomorphic-unfetch';
 import React from 'react'
 
-const Index = ({musicData}) => {
-    console.log(musicData)
+const Index = ({productData}) => {
+    console.log(productData)
     return (
         <Layout>
             <div>
                 <h1>Welcome to Next Application</h1>
-                <h3>Songs List</h3>
-                {musicData.map((item, i) => {
+                <h3>Product List</h3>
+                {productData.map((item, i) => {
                    return (
-                   <li key={i}>{item.title}</li>
+                       <>
+                   <li key={i}>{item.name}</li>
+                   </>
                    )
                })}
             </div>
@@ -20,9 +22,10 @@ const Index = ({musicData}) => {
 }
 
 Index.getInitialProps = async function() {
-    const response = await fetch(`https://www.what-song.com/api/recent-movies`);
+    const response = await fetch(`https://gorest.co.in/public-api/products`);
+    console.log(response)
     const result = await response.json();
-    return { musicData: result.data }
+    return { productData: result.data }
 }
 
 export default Index;
